665e9f3fae78b2682380f75559e4d47c4a6ef8c1
Christina Nuncio <nuncio.christina01@gmail.com>
2021-09-29 09:45:34 -0500

    MAVEN first exercise

---
+++ src/main/java/aClass.java
@@ -0,0 +1,13 @@
+import org.apache.commons.lang3.StringUtils;
+import java.util.Scanner;
+public class aClass {
+    public static void main(String args[]) {
+Scanner sc = new Scanner(System.in);
+        System.out.println("Enter message: " );
+        String userInput = sc.nextLine();
+        System.out.println(StringUtils.isNumeric(userInput));
+        System.out.println(StringUtils.swapCase(userInput));
+        System.out.println(StringUtils.reverse(userInput));
+    }
+
+}
